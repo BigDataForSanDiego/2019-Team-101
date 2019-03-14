12056e29712921ff06a465b1a53777e808289053
magdalenan <MagdalenaN@ashn.com>
2019-03-14 14:53:52 -0700

    sdaf

---
+++ CityPark/CityPark/CityPark/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace CityPark.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public ICommand NavigateMaps { get; set; }
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page!";
+            NavigateMaps = new DelegateCommand(NavigateHandler);
+        }
+
+        public void NavigateHandler()
+        {
+        }
+    }
+}
